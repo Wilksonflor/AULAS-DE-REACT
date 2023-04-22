09d21dd828ab3cdcceb7bc13049ca8e6ae045243
Wilkson Flor <wilksonflor12@gmail.com>
2023-04-22 15:54:44 -0300

    Corrigir o erro do Button para criar projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,57 +1,66 @@
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 import Container from "./layout/Container";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-        <li>Home</li>
-        <li>Contato</li>
+      <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+        <Navbar />
+        
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home customClass="min-height" />} />
+            <Route
+              path="/company"
+              element={
+                <React.Fragment>
+                  <Container />
+                  <Company />
+                </React.Fragment>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <React.Fragment>
+                  <Container />
+                  <Contact />
+                </React.Fragment>
+              }
+            />
+            <Route
+              path="/newproject"
+              element={
+                <React.Fragment>
+                  <Container />
+                  <NewProject />
+                </React.Fragment>
+              }
+            />
+
+            <Route
+              path="/projects"
+              element={
+                <React.Fragment>
+                  <Container />
+                  <Projects />
+                </React.Fragment>
+              }
+            />
+          </Routes>
+        </div>
+
+        <Footer />
       </div>
-
-      <Routes>
-        <Route path="/" element={<Home customClass="min-height"/>} />
-        <Route
-          path="/company"
-          element={
-            <React.Fragment >
-              <Container />
-              <Company />
-            </React.Fragment>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <React.Fragment>
-              <Container />
-              <Contact />
-            </React.Fragment>
-          }
-        />
-        <Route
-          path="/newproject"
-          element={
-            <React.Fragment>
-              <Container />
-              <NewProject />
-            </React.Fragment>
-          }
-        />
-      </Routes>
-
-      <p>Footer</p>
     </Router>
   );
 }
